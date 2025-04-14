880b6d5e442a5f2452d4a0e3b7b92565954b6dab
Priyanka Mehta <primehta17@gmail.com>
2025-04-14 12:50:42 +0530

    revert changes

--- frontend/src/Pages/Search.js
+++ frontend/src/Pages/Search.js
@@ -3,20 +3,18 @@ function Search(){
     return(
         <div className="container searchContainer mt-5">
             <div className="row">
-                <form action="./" method="POST">
-                    <div className="col-md-3">
-                        <input className="inputs" placeholder="Location" />
-                    </div>
-                    <div className="col-md-3">
-                        <input className="inputs" placeholder="Type" />
-                    </div>
-                    <div className="col-md-3">
-                        <input className="inputs" placeholder="Price Range" />
-                    </div>
-                    <div className="col-md-3">
-                        <button className="signup">Sign up</button>
-                    </div>  
-                </form>
+            <div className="col-md-3">
+                <input className="inputs" placeholder="Location" />
+            </div>
+            <div className="col-md-3">
+                <input className="inputs" placeholder="Type" />
+            </div>
+            <div className="col-md-3">
+                <input className="inputs" placeholder="Price Range" />
+            </div>
+            <div className="col-md-3">
+                <button className="signup">Sign up</button>
+            </div>  
             </div>
         </div>
     )
