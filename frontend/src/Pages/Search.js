d2e082f5880309c4335342e7ff7bbef38eb13ba1
Priyanka Mehta <primehta17@gmail.com>
2025-04-14 12:45:32 +0530

    This commit is a mistake

--- frontend/src/Pages/Search.js
+++ frontend/src/Pages/Search.js
@@ -3,18 +3,20 @@ function Search(){
     return(
         <div className="container searchContainer mt-5">
             <div className="row">
-            <div className="col-md-3">
-                <input className="inputs" placeholder="Location" />
-            </div>
-            <div className="col-md-3">
-                <input className="inputs" placeholder="Type" />
-            </div>
-            <div className="col-md-3">
-                <input className="inputs" placeholder="Price Range" />
-            </div>
-            <div className="col-md-3">
-                <button className="signup">Sign up</button>
-            </div>  
+                <form action="./" method="POST">
+                    <div className="col-md-3">
+                        <input className="inputs" placeholder="Location" />
+                    </div>
+                    <div className="col-md-3">
+                        <input className="inputs" placeholder="Type" />
+                    </div>
+                    <div className="col-md-3">
+                        <input className="inputs" placeholder="Price Range" />
+                    </div>
+                    <div className="col-md-3">
+                        <button className="signup">Sign up</button>
+                    </div>  
+                </form>
             </div>
         </div>
     )
